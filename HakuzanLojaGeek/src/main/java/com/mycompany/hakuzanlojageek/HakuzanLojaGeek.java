585d5569ebe5c02bc65c48dfdeb4cf54b917fc03
Everton <76123738+Evert0nLuiz@users.noreply.github.com>
2023-09-20 20:39:06 -0300

    Add files via upload

---
+++ HakuzanLojaGeek/src/main/java/com/mycompany/hakuzanlojageek/HakuzanLojaGeek.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hakuzanlojageek;
+
+import com.mycompany.hakuzanlojageek.view.TelaLogin;
+
+/**
+ *
+ * @author everton.lcsousa
+ */
+public class HakuzanLojaGeek {
+
+    public static void main(String[] args) {
+        TelaLogin tlg = new TelaLogin();
+        
+        tlg.setVisible(true);
+        
+    }
+}
