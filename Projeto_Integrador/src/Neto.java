8b543d0da6362f8935114f352750a6b2a26e1ceb
ANTONIO JOAQUIM DA SILVA NETO <antonio.jsneto9@senacsp.edu.br>
2023-09-28 20:18:17 -0300

    Classes Java

--- Projeto_Integrador/src/Neto.java
+++
@@ -1,5 +0,0 @@
-public class Neto {
-    public static void main(String[] args){
-        System.out.println("HELLO WORD!");
-    }
-}
--- Projeto_Integrador/src/caio.java
+++
@@ -1,9 +0,0 @@
-public class caio {
-    public static void main(String[] args) {
-
-          System.out.println("Caio Figueiredo");
-
-        System.out.println("Oi");
-
-    }
-}
