1629ba447e213a8c12f39c8822fe274b511b1686
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-10-31 13:14:45 -0300

    CRUD itens pedido

--- HakuzanLoja/src/main/java/com/projetopi/hakuzanloja/controller/ItensPedidoDao.java
+++ HakuzanLoja/src/main/java/com/projetopi/hakuzanloja/controller/ItensPedidoDao.java
@@ -12,6 +12,7 @@ import javax.swing.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,15 +47,78 @@ public class ItensPedidoDao extends ConectarDao implements CrudDao<ItemPedido> {
     @Override
     public void cadastrar(ItemPedido itemPedido) {
 
+        String sql = "INSERT INTO TB_ITENSPEDIDO (FK_PRODUTO, QT_PRODUTO, VL_UNITARIO, VL_TOTAL, FK_PEDIDO) " +
+                " VALUES (?, ?, ?, ?, ?)";
+
+        try{
+
+            double total = itemPedido.getProduto().getValor() * itemPedido.getQuantidade();
+
+            PreparedStatement ps = (PreparedStatement) getConexao().prepareStatement(sql);
+            ps.setLong(1, itemPedido.getProduto().getId());
+            ps.setInt(2, itemPedido.getQuantidade());
+            ps.setDouble(3, itemPedido.getProduto().getValor());
+            ps.setDouble(4, total);
+            ps.setLong(5, itemPedido.getPedido().getId());
+
+            ps.execute();
+
+        }catch (SQLException err) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar item no pedido. \n"
+                    + itemPedido.getProduto().getProduto() + "\n"
+                    + err.getMessage());
+        }
+
     }
 
     @Override
     public void editar(ItemPedido itemPedido) {
 
+        String sql = "UPDATE TB_ITENSPEDIDO QT_PRODUTO=?, VL_UNITARIO=?, VL_TOTAL=?) WHERE PK_ID=? ";
+
+        try{
+
+            double total = itemPedido.getProduto().getValor() * itemPedido.getQuantidade();
+
+            PreparedStatement ps = (PreparedStatement) getConexao().prepareStatement(sql);
+            ps.setInt(1, itemPedido.getQuantidade());
+            ps.setDouble(2, itemPedido.getValorUnitario());
+            ps.setDouble(3, total);
+            ps.setLong(4, itemPedido.getId());
+
+            ps.execute();
+
+            JOptionPane.showMessageDialog(null, "Pedido editado com sucesso.");
+
+        }catch (SQLException err) {
+            JOptionPane.showMessageDialog(null, "Erro ao editar item. \n"
+                    + itemPedido.getProduto().getProduto() + "\n"
+                    + err.getMessage());
+        }
+
+
     }
 
     @Override
     public void excluir(ItemPedido itemPedido) {
+
+        String sql = "DELETE FROM TB_ITENSPEDIDO WHERE PK_ID=?";
+
+        try{
+
+            PreparedStatement ps = (PreparedStatement) getConexao().prepareStatement(sql);
+            ps.setLong(1, itemPedido.getId());
+
+            ps.execute();
+
+            JOptionPane.showMessageDialog(null, "Item deletado com sucesso.");
+
+        }catch (SQLException err) {
+            JOptionPane.showMessageDialog(null, "Erro ao deletar item. \n"
+                    + itemPedido.getProduto().getProduto() + "\n"
+                    + err.getMessage());
+        }
+
 
     }
 
