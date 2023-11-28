804a78b137fcff224661decd26b288319448425a
Everton L C Sousa <evertonluizdacruzsousa@hotmail.com>
2023-11-27 22:56:24 -0300

    Mostra Lista de produtos

---
+++ HakuzanLoja/src/main/java/com/projetopi/hakuzanloja/model/ProdutosAtual.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.projetopi.hakuzanloja.model;
+
+import com.projetopi.hakuzanloja.controller.ProdutoDao;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author snubd
+ */
+public class ProdutosAtual {
+    private static List<Produto> listaProd;
+
+    public static List<Produto> getListaProd() {
+        return listaProd;
+    }
+
+    public static void setListaProd(List<Produto> listaProd) {
+        ProdutosAtual.listaProd = listaProd;
+    }
+    
+    
+    
+    
+}
